a64f10f4faaff62d313c13d1b0897155ffa3817b
cluis11 <cluis.9405@gmail.com>
2026-06-18 23:47:41 -0600

    script

--- NutriTEC.API/Services/NutricionistaService.cs
+++ NutriTEC.API/Services/NutricionistaService.cs
@@ -1,3 +1,4 @@
+using BCrypt.Net;
 using NutriTEC.API.Data.Repositories;
 using NutriTEC.API.Models;
 using NutriTEC.API.DTOs;
@@ -13,9 +14,27 @@ namespace NutriTEC.API.Services
             _nutricionistaRepository = nutricionistaRepository;
         }
 
-        public Task<int> Registro(Nutricionista nutricionista, string foto_base64) => throw new NotImplementedException();
-        public Task<Nutricionista> ObtenerPerfil(int id) => throw new NotImplementedException();
-        public Task ActualizarPerfil(int id, Nutricionista nutricionista, string foto_base64) => throw new NotImplementedException();
+        public async Task<int> Registro(Nutricionista nutricionista)
+        {
+            if (await _nutricionistaRepository.CorreoExiste(nutricionista.Correo))
+                throw new InvalidOperationException("El correo ya está registrado");
+
+            nutricionista.Contrasena = BCrypt.Net.BCrypt.HashPassword(nutricionista.Contrasena);
+            nutricionista.Foto = null;
+
+            return await _nutricionistaRepository.CrearNutricionista(nutricionista);
+        }
+
+        public async Task<Nutricionista> ObtenerPerfil(int id)
+        {
+            var nutricionista = await _nutricionistaRepository.ObtenerNutricionista(id);
+
+            if (nutricionista == null)
+                throw new KeyNotFoundException("Nutricionista no encontrado");
+
+            return nutricionista;
+        }
+        public Task ActualizarPerfil(int id, Nutricionista nutricionista) => throw new NotImplementedException();
         public Task<List<Cliente>> ObtenerPacientes(int id_nutricionista) => throw new NotImplementedException();
         public Task<List<ClienteDisponibleDTO>> ObtenerPacientesDisponibles() => throw new NotImplementedException();
         public Task AsociarPaciente(int id_nutricionista, int id_cliente) => throw new NotImplementedException();
